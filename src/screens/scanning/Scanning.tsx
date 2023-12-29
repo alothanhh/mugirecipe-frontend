dd633dd91737dc2491c0e2e4d120f978e389d32c
Quang Thành <thanh.dangquang28@gmail.com>
2023-12-29 21:31:20 +0700

    add scanning page (can flip camera and take picture)

--- src/screens/scanning/Scanning.tsx
+++ src/screens/scanning/Scanning.tsx
@@ -1,12 +1,102 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import React, { useState, useEffect, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import colors from '@/constants/colors';
 
 const Scanning = () => {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  // const [uri, setUri] = useState();
+
+  const ref = useRef(null)
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  const _takePhoto = async () => {
+    if (ref.current) {
+      const photo = await (ref.current as any).takePictureAsync();
+      console.log(photo)
+      // setUri(photo.uri);
+    } else {
+      console.error('Ref is null.');
+    }
+  }
+
   return (
-    <View>
-      <Text>Scanning screen!</Text>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={ref}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={_takePhoto}
+          >
+            <Text style={styles.text}>Snap Photo</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {/* Preview Image */}
+      {/* <Image
+        className="w-[208px] h-[204px] rounded-[15px]"
+        source={{
+          uri: uri
+        }}
+      /> */}
     </View>
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: colors.lightred,
+    margin: 5,
+    padding: 15,
+    borderRadius: 20,
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+});
+
 
 export default Scanning;
